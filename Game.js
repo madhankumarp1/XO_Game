a281576a7b58d4498865ecb4b39028c2bb073bf6
madhan kumar <140236304+madhankumarp1@users.noreply.github.com>
2024-05-20 15:57:30 +0530

    add

---
+++ Game.js
@@ -0,0 +1,207 @@
+
+let W = window.innerWidth;
+let H = window.innerHeight;
+const canvas = document.getElementById("canvas");
+const context = canvas.getContext("2d");
+const maxConfettis = 150;
+const particles = [];
+
+const possibleColors = [
+  "DodgerBlue",
+  "OliveDrab",
+  "Gold",
+  "Pink",
+  "SlateBlue",
+  "LightBlue",
+  "Gold",
+  "Violet",
+  "PaleGreen",
+  "SteelBlue",
+  "SandyBrown",
+  "Chocolate",
+  "Crimson"
+];
+
+function randomFromTo(from, to) {
+  return Math.floor(Math.random() * (to - from + 1) + from);
+}
+
+function confettiParticle() {
+  this.x = Math.random() * W; // x
+  this.y = Math.random() * H - H; // y
+  this.r = randomFromTo(11, 33); // radius
+  this.d = Math.random() * maxConfettis + 11;
+  this.color =
+    possibleColors[Math.floor(Math.random() * possibleColors.length)];
+  this.tilt = Math.floor(Math.random() * 33) - 11;
+  this.tiltAngleIncremental = Math.random() * 0.07 + 0.05;
+  this.tiltAngle = 0;
+
+  this.draw = function() {
+    context.beginPath();
+    context.lineWidth = this.r / 2;
+    context.strokeStyle = this.color;
+    context.moveTo(this.x + this.tilt + this.r / 3, this.y);
+    context.lineTo(this.x + this.tilt, this.y + this.tilt + this.r / 5);
+    return context.stroke();
+  };
+}
+
+function Draw() {
+  const results = [];
+
+  // Magical recursive functional love
+  requestAnimationFrame(Draw);
+
+  context.clearRect(0, 0, W, window.innerHeight);
+
+  for (var i = 0; i < maxConfettis; i++) {
+    results.push(particles[i].draw());
+  }
+
+  let particle = {};
+  let remainingFlakes = 0;
+  for (var i = 0; i < maxConfettis; i++) {
+    particle = particles[i];
+
+    particle.tiltAngle += particle.tiltAngleIncremental;
+    particle.y += (Math.cos(particle.d) + 3 + particle.r / 2) / 2;
+    particle.tilt = Math.sin(particle.tiltAngle - i / 3) * 15;
+
+    if (particle.y <= H) remainingFlakes++;
+
+    // If a confetti has fluttered out of view,
+    // bring it back to above the viewport and let if re-fall.
+    if (particle.x > W + 30 || particle.x < -30 || particle.y > H) {
+      particle.x = Math.random() * W;
+      particle.y = -30;
+      particle.tilt = Math.floor(Math.random() * 10) - 20;
+    }
+  }
+
+  return results;
+}
+
+window.addEventListener(
+  "resize",
+  function() {
+    W = window.innerWidth;
+    H = window.innerHeight;
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+  },
+  false
+);
+
+// Push new confetti objects to `particles[]`
+for (var i = 0; i < maxConfettis; i++) {
+  particles.push(new confettiParticle());
+}
+
+// Initialize
+canvas.width = W;
+canvas.height = H;
+Draw();
+// 
+
+const allCards = document.querySelectorAll('.cell');
+let reset = document.getElementById('reset');
+let nextgame = document.getElementById('nextgame');
+let currentPlayer = 'X';
+let gameActive = true;
+
+const set_of_pairs = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],
+    [0, 3, 6], [1, 4, 7], [2, 5, 8], 
+    [0, 4, 8], [2, 4, 6]             
+];
+// console.log([...allCards]);
+
+allCards.forEach((card) => {
+    card.addEventListener('click', () => {
+        if (gameActive && card.textContent.trim() === '') {
+            card.textContent = currentPlayer;
+
+            if (currentPlayer === 'X') {
+                 card.style.backgroundColor = 'yellow';
+
+            } else {
+                card.style.backgroundColor = 'red';
+
+            }
+            // Check for winner
+            if (checkWinner()) {
+                document.getElementById('answer').textContent = `Player ${currentPlayer} wins!`;
+                document.getElementById('answe_next').textContent = `Player ${currentPlayer} wins!`;
+
+                document.getElementById('con').style.display = 'none';
+                document.getElementById('party-info').style.display = 'block';
+                document.getElementById('canvas').style.display = 'block';
+
+                document.getElementById('party-info').style.display = 'block';
+     
+
+                gameActive = false;
+
+            } else if ([...allCards].every(cell => cell.textContent.trim() !== '') ) {
+                // console.log([...allCards]);
+                // console.log([...allCards].every(cell => cell.textContent.trim() !== ''));
+                document.getElementById('answe_next_head').textContent = 'Game is It\'s a tie!!!';
+
+
+                document.getElementById('answer').textContent = 'It\'s a tie!';
+
+                document.getElementById('answe_next').textContent = 'It\'s a tie!';
+                
+       
+                document.getElementById('con').style.display = 'none';
+                document.getElementById('party-info').style.display = 'block';
+          
+                document.getElementById('party-info').style.display = 'block';
+
+                gameActive = false;
+
+            } 
+            else {
+                if (currentPlayer === 'X') {
+                    currentPlayer = 'O';
+                } else {
+                    currentPlayer = 'X';
+
+                }
+                document.getElementById('answer').textContent = `Player ${currentPlayer}'s turn`;
+            }
+        }
+    });
+});
+function checkWinner() {
+    for (let  pairs  of set_of_pairs) {
+        const [a, b, c] = pairs;
+        // console.log(pairs);
+        if (
+            allCards[a].textContent.trim() !== '' &&
+            allCards[a].textContent === allCards[b].textContent &&
+            allCards[a].textContent === allCards[c].textContent
+        ) {
+            return true;
+        }
+    }
+    return false;
+}
+
+
+
+
+reset.addEventListener('click', reset_page);
+function reset_page(){
+    window.location.reload();
+}
+nextgame.addEventListener('click', game_rest);
+function game_rest(){
+    window.location.reload();
+}
+
+
+
+
+
